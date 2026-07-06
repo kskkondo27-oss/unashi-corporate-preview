c26980492a11c7d4f77b17d76bf8e7d989829199
圭祐 <ksk.kondo.27@gmail.com>
2026-07-06 20:53:29 +0900

    Align copy metrics with latest guidance
    # Conflicts:
    #	assets/js/site-data.js
    #	business/bgm/index.html
    #	business/index.html
    #	index.html

--- assets/js/site-data.js
+++ assets/js/site-data.js
@@ -2,16 +2,16 @@ window.UNASHI_SITE_DATA = {
   metrics: {
     founded: "2023年設立",
     businessCount: "3事業",
-    totalChannels: "累計3,000ch+",
-    maClosed: "成約150件+",
-    maHandled: "取扱1,000ch+",
+    totalChannels: "累計3,000ch超",
+    maClosed: "成約150件超",
+    maHandled: "取扱1,000ch超",
     maMaxDeal: "最大2億円",
     maTotalVolume: "累計取扱50億円規模",
     maAverageDeal: "平均単価1,000万円超",
     bgmMilestone: "累計再生100億回突破",
     bgmMonthly: "提供規模拡大中",
-    bgmChannels: "1,000ch+ に提供",
-    bgmSongs: "1,000曲+",
+    bgmChannels: "1,000ch超に提供",
+    bgmSongs: "1,000曲超",
     bgmRevenueShare: "クリエイター70% / ウナシ30%"
   },
   links: {
